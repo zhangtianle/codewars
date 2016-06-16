2d37fe454014d18864c83b5cb2101c5aa972adbb
TianleZhang <cqtianle@163.com>
2016-06-16 23:03:05 +0800

    TheClockwiseSpiral

--- src/five/TheClockwiseSpiral.java
+++ src/five/TheClockwiseSpiral.java
@@ -21,7 +21,53 @@ package five;
 public class TheClockwiseSpiral {
 
     public static int[][] createSpiral(int N) {
+        int num = 1;
+        int maxNum = N * N;
 
-        return null;
+        int[][] r = new int[N][N];
+
+        int[] direction = new int[]{1,2,3,4};
+        int d = 0;
+
+        int i = 0;
+        int j = 0;
+
+        int right = N-1;
+        int top = 1;
+        int left = 0;
+        int bottom = N-1;
+
+        while (num <= maxNum) {
+
+            while (d == 0 && j <=right) {
+                r[i][j++] = num++;
+            }
+            right--;
+            d++;
+            j--;
+            i++;
+            while (d == 1 && i <= bottom) {
+                r[i++][j] = num++;
+            }
+            bottom--;
+            d++;
+            i--;
+            j--;
+            while (d == 2 && j>=left) {
+                r[i][j--] = num++;
+            }
+            left++;
+            d++;
+            j++;
+            i--;
+            while (d == 3 && i>=top) {
+                r[i--][j] = num++;
+            }
+            top++;
+            d = 0;
+            i++;
+            j++;
+        }
+        return r;
     }
 }
---
+++ test/TheClockwiseSpiralTest.java
@@ -0,0 +1,19 @@
+import five.TheClockwiseSpiral;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TheClockwiseSpiralTest {
+
+    TheClockwiseSpiral spiral = new TheClockwiseSpiral();
+
+    @Test
+    public void should_test_spiral() {
+        int[][] expected = new int[][]{
+                {1, 2, 3},
+                {8, 9, 4},
+                {7, 6, 5}};
+
+        Assert.assertArrayEquals(expected, spiral.createSpiral(3));
+
+    }
+}
